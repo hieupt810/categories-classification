2b5584c6859c480c437fd52f2de6f52ea4b419f7
wai157 <hoang150703@gmail.com>
2024-12-10 20:59:24 +0700

    train test pipeline

--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -24,57 +24,32 @@ to_2tuple = _ntuple(2)
 class PatchEmbed(nn.Module):
     def __init__(
         self,
-        img_size: Optional[int] = 224,
+        img_size: int = 224,
         patch_size: int = 16,
         in_chans: int = 3,
         embed_dim: int = 768,
-        norm_layer: Optional[Callable] = None,
         bias: bool = True,
-        strict_img_size: bool = True,
     ):
         super().__init__()
         self.patch_size = to_2tuple(patch_size)
         self.img_size, self.grid_size, self.num_patches = self._init_img_size(img_size)
 
-        self.strict_img_size = strict_img_size
-
-        self.proj = nn.Conv2d(
-            in_chans, embed_dim, kernel_size=patch_size, stride=patch_size, bias=bias
-        )
-        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size, bias=bias)
 
     def _init_img_size(self, img_size: Union[int, Tuple[int, int]]):
         assert self.patch_size
-        if img_size is None:
-            return None, None, None
         img_size = to_2tuple(img_size)
         grid_size = tuple([s // p for s, p in zip(img_size, self.patch_size)])
         num_patches = grid_size[0] * grid_size[1]
         return img_size, grid_size, num_patches
 
-    def feat_ratio(self, as_scalar=True) -> Union[Tuple[int, int], int]:
-        if as_scalar:
-            return max(self.patch_size)
-        else:
-            return self.patch_size
-
-    def dynamic_feat_size(self, img_size: Tuple[int, int]) -> Tuple[int, int]:
-        return img_size[0] // self.patch_size[0], img_size[1] // self.patch_size[1]
-
     def forward(self, x):
         B, C, H, W = x.shape
-        if self.img_size is not None:
-            if self.strict_img_size:
-                assert (
-                    H == self.img_size[0]
-                ), f"Input height ({H}) doesn't match model ({self.img_size[0]})."
-                assert (
-                    W == self.img_size[1]
-                ), f"Input width ({W}) doesn't match model ({self.img_size[1]})."
-
+        assert H == self.img_size[0], f"Input height ({H}) doesn't match model ({self.img_size[0]})."
+        assert W == self.img_size[1], f"Input width ({W}) doesn't match model ({self.img_size[1]})."
+            
         x = self.proj(x)
         x = x.flatten(2).transpose(1, 2)  # NCHW -> NLC
-        x = self.norm(x)
         return x
 
 
@@ -86,7 +61,7 @@ class Mlp(nn.Module):
         out_features=None,
         act_layer=nn.GELU,
         bias=True,
-        drop=0.0,
+        drop=0.,
     ):
         super().__init__()
         out_features = out_features or in_features
@@ -116,10 +91,8 @@ class Attention(nn.Module):
         dim: int,
         num_heads: int = 8,
         qkv_bias: bool = False,
-        qk_norm: bool = False,
-        attn_drop: float = 0.0,
-        proj_drop: float = 0.0,
-        norm_layer: nn.Module = nn.LayerNorm,
+        attn_drop: float = 0.,
+        proj_drop: float = 0.,
     ) -> None:
         super().__init__()
         assert dim % num_heads == 0, "dim should be divisible by num_heads"
@@ -128,8 +101,6 @@ class Attention(nn.Module):
         self.scale = self.head_dim**-0.5
 
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
-        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
@@ -142,7 +113,6 @@ class Attention(nn.Module):
             .permute(2, 0, 3, 1, 4)
         )
         q, k, v = qkv.unbind(0)
-        q, k = self.q_norm(q), self.k_norm(k)
 
         x = F.scaled_dot_product_attention(
             q,
@@ -162,11 +132,10 @@ class Block(nn.Module):
         self,
         dim: int,
         num_heads: int,
-        mlp_ratio: float = 4.0,
+        mlp_ratio: float = 4.,
         qkv_bias: bool = False,
-        qk_norm: bool = False,
-        proj_drop: float = 0.0,
-        attn_drop: float = 0.0,
+        proj_drop: float = 0.,
+        attn_drop: float = 0.,
         act_layer: nn.Module = nn.GELU,
         norm_layer: nn.Module = nn.LayerNorm,
         mlp_layer: nn.Module = Mlp,
@@ -177,13 +146,9 @@ class Block(nn.Module):
             dim,
             num_heads=num_heads,
             qkv_bias=qkv_bias,
-            qk_norm=qk_norm,
             attn_drop=attn_drop,
             proj_drop=proj_drop,
-            norm_layer=norm_layer,
         )
-        self.ls1 = nn.Identity()
-        self.drop_path1 = nn.Identity()
 
         self.norm2 = norm_layer(dim)
         self.mlp = mlp_layer(
@@ -192,39 +157,11 @@ class Block(nn.Module):
             act_layer=act_layer,
             drop=proj_drop,
         )
-        self.ls2 = nn.Identity()
-        self.drop_path2 = nn.Identity()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
-        x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
         return x
-
-
-def global_pool_nlc(
-    x: torch.Tensor,
-    pool_type: str = "token",
-    num_prefix_tokens: int = 1,
-    reduce_include_prefix: bool = False,
-):
-    if not pool_type:
-        return x
-
-    if pool_type == "token":
-        x = x[:, 0]  # class token
-    else:
-        x = x if reduce_include_prefix else x[:, num_prefix_tokens:]
-        if pool_type == "avg":
-            x = x.mean(dim=1)
-        elif pool_type == "avgmax":
-            x = 0.5 * (x.amax(dim=1) + x.mean(dim=1))
-        elif pool_type == "max":
-            x = x.amax(dim=1)
-        else:
-            assert not pool_type, f"Unknown pool type {pool_type}"
-
-    return x
-
 
 class VisionTransformer(nn.Module):
     def __init__(
@@ -232,23 +169,18 @@ class VisionTransformer(nn.Module):
         img_size: Union[int, Tuple[int, int]] = 224,
         patch_size: Union[int, Tuple[int, int]] = 16,
         in_chans: int = 3,
-        num_classes: int = 1000,
-        global_pool: Literal["", "avg", "avgmax", "max", "token"] = "token",
+        num_classes: int = 10,
         embed_dim: int = 768,
         depth: int = 12,
         num_heads: int = 12,
-        mlp_ratio: float = 4.0,
+        mlp_ratio: float = 4.,
         qkv_bias: bool = True,
-        qk_norm: bool = False,
-        class_token: bool = True,
-        drop_rate: float = 0.0,
-        pos_drop_rate: float = 0.0,
-        proj_drop_rate: float = 0.0,
-        attn_drop_rate: float = 0.0,
+        drop_rate: float = 0.,
+        pos_drop_rate: float = 0.,
+        proj_drop_rate: float = 0.,
+        attn_drop_rate: float = 0.,
         embed_layer: Callable = PatchEmbed,
-        norm_layer: Optional[Union[str, Callable, Type[torch.nn.Module]]] = partial(
-            nn.LayerNorm, eps=1e-6
-        ),
+        norm_layer: Optional[Union[str, Callable, Type[torch.nn.Module]]] = partial(nn.LayerNorm, eps=1e-6),
         act_layer: Optional[Union[str, Callable, Type[torch.nn.Module]]] = nn.GELU,
         block_fn: Type[nn.Module] = Block,
         mlp_layer: Type[nn.Module] = Mlp,
@@ -259,31 +191,25 @@ class VisionTransformer(nn.Module):
             patch_size: Patch size.
             in_chans: Number of image input channels.
             num_classes: Number of classes for classification head.
-            global_pool: Type of global pooling for final sequence (default: 'token').
             embed_dim: Transformer embedding dimension.
             depth: Depth of transformer.
             num_heads: Number of attention heads.
             mlp_ratio: Ratio of mlp hidden dim to embedding dim.
             qkv_bias: Enable bias for qkv projections if True.
-            class_token: Use class token.
-            final_norm: Enable norm after transformer blocks, before head (standard in most ViT).
             drop_rate: Head dropout rate.
             pos_drop_rate: Position embedding dropout rate.
+            proj_drop_rate: Projection dropout rate.
             attn_drop_rate: Attention dropout rate.
             embed_layer: Patch embedding layer.
+            norm_layer: Normalization layer.
+            act_layer: MLP activation layer.
             block_fn: Transformer block layer.
+            mlp_layer: MLP layer.
         """
         super().__init__()
-        assert global_pool in ("", "avg", "avgmax", "max", "token")
-        assert class_token or global_pool != "token"
-
         self.num_classes = num_classes
-        self.global_pool = global_pool
-        self.num_features = self.head_hidden_size = self.embed_dim = (
-            embed_dim  # for consistency with other models
-        )
-        self.num_prefix_tokens = 1 if class_token else 0
-        self.has_class_token = class_token
+        self.num_features = self.head_hidden_size = self.embed_dim = embed_dim
+        self.num_prefix_tokens = 1
 
         self.patch_embed = embed_layer(
             img_size=img_size,
@@ -292,40 +218,25 @@ class VisionTransformer(nn.Module):
             embed_dim=embed_dim,
         )
         num_patches = self.patch_embed.num_patches
-        reduction = (
-            self.patch_embed.feat_ratio()
-            if hasattr(self.patch_embed, "feat_ratio")
-            else patch_size
-        )
 
-        self.cls_token = (
-            nn.Parameter(torch.zeros(1, 1, embed_dim)) if class_token else None
-        )
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         embed_len = num_patches + self.num_prefix_tokens
         self.pos_embed = nn.Parameter(torch.randn(1, embed_len, embed_dim) * 0.02)
         self.pos_drop = nn.Dropout(p=pos_drop_rate)
 
-        self.blocks = nn.Sequential(
-            *[
-                block_fn(
-                    dim=embed_dim,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    qk_norm=qk_norm,
-                    proj_drop=proj_drop_rate,
-                    attn_drop=attn_drop_rate,
-                    norm_layer=norm_layer,
-                    act_layer=act_layer,
-                    mlp_layer=mlp_layer,
-                )
-                for _ in range(depth)
-            ]
-        )
-        self.feature_info = [
-            dict(module=f"blocks.{i}", num_chs=embed_dim, reduction=reduction)
-            for i in range(depth)
-        ]
+        self.blocks = nn.Sequential(*[
+            block_fn(
+                dim=embed_dim,
+                num_heads=num_heads,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                proj_drop=proj_drop_rate,
+                attn_drop=attn_drop_rate,
+                norm_layer=norm_layer,
+                act_layer=act_layer,
+                mlp_layer=mlp_layer,
+            )
+            for _ in range(depth)])
         self.norm = norm_layer(embed_dim)
 
         # Classifier Head
@@ -334,15 +245,8 @@ class VisionTransformer(nn.Module):
 
     def _pos_embed(self, x: torch.Tensor) -> torch.Tensor:
         pos_embed = self.pos_embed
-
-        to_cat = []
-        if self.cls_token is not None:
-            to_cat.append(self.cls_token.expand(x.shape[0], -1, -1))
-
-        if to_cat:
-            x = torch.cat(to_cat + [x], dim=1)
+        x = torch.cat([self.cls_token.expand(x.shape[0], -1, -1)] + [x], dim=1)
         x = x + pos_embed
-
         return self.pos_drop(x)
 
     def forward_features(self, x: torch.Tensor) -> torch.Tensor:
@@ -353,15 +257,12 @@ class VisionTransformer(nn.Module):
         return x
 
     def pool(self, x: torch.Tensor) -> torch.Tensor:
-        x = global_pool_nlc(
-            x, pool_type=self.global_pool, num_prefix_tokens=self.num_prefix_tokens
-        )
-        return x
+        return x[:, 0]
 
-    def forward_head(self, x: torch.Tensor, pre_logits: bool = False) -> torch.Tensor:
+    def forward_head(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pool(x)
         x = self.head_drop(x)
-        return x if pre_logits else self.head(x)
+        return self.head(x)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.forward_features(x)
